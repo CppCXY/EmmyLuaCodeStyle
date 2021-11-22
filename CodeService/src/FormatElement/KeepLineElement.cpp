cf7bb1d8b2ab59d11a5cff97175b48808bbbd764
CppCXY <812125110@qq.com>
2021-11-23 01:55:51 +0800

    修复一个算法结果不稳定得bug

--- CodeService/src/FormatElement/KeepLineElement.cpp
+++ CodeService/src/FormatElement/KeepLineElement.cpp
@@ -14,17 +14,17 @@ FormatElementType KeepLineElement::GetType()
 
 void KeepLineElement::Serialize(FormatContext& ctx, int position, FormatElement& parent)
 {
+	int lastElementLine = getLastValidLine(ctx, position, parent);
+	int nextElementLine = getNextValidLine(ctx, position, parent);
+
+	if (nextElementLine == -1)
+	{
+		return;
+	}
+
 	int line = _line;
 	if (line == -1)
 	{
-		int lastElementLine = getLastValidLine(ctx, position, parent);
-		int nextElementLine = getNextValidLine(ctx, position, parent);
-
-		if (nextElementLine == -1)
-		{
-			return;
-		}
-
 		line = nextElementLine - lastElementLine - 1;
 		if (line < 0)
 		{
@@ -37,16 +37,18 @@ void KeepLineElement::Serialize(FormatContext& ctx, int position, FormatElement&
 
 void KeepLineElement::Diagnosis(DiagnosisContext& ctx, int position, FormatElement& parent)
 {
+	ctx.SetCharacterCount(0);
+
+	int lastElementOffset = getLastValidOffset(position, parent);
+	int nextElementOffset = getNextValidOffset(position, parent);
+
+	if (nextElementOffset == -1)
+	{
+		return;
+	}
+
 	if (_line != -1)
 	{
-		int lastElementOffset = getLastValidOffset(position, parent);
-		int nextElementOffset = getNextValidOffset(position, parent);
-
-		if (nextElementOffset == -1)
-		{
-			return;
-		}
-
 		int lastElementLine = ctx.GetLine(lastElementOffset);
 		int nextElementLine = ctx.GetLine(nextElementOffset);
 
@@ -56,6 +58,4 @@ void KeepLineElement::Diagnosis(DiagnosisContext& ctx, int position, FormatEleme
 			                  TextRange(lastElementOffset, nextElementOffset));
 		}
 	}
-	ctx.SetCharacterCount(0);
-
 }
