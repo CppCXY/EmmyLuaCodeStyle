af8bbcbd688826e1e0a48fff969677b48689e260
CppCXY <812125110@qq.com>
2021-12-21 14:05:04 +0800

    提供更多的测试

---
+++ Test/test_script/format_text/table.lua
@@ -0,0 +1,52 @@
+local t = {
+    aa,bbb,ccc,ddd,eee,fff
+}
+local t2 = { 1, 2, 3, 4, 5 }
+
+local t3 = { 123131,
+aaa,bbbb,ccc,1,2,3,4,5
+}
+
+local t4 = {
+    aaa = 1,
+    bbbb = 4456,
+    ["wgfwwfw"] = 890,
+    [{}] = 131,
+    [function() end] = 131,
+    [1231] = 12321
+}
+
+local t5 = {
+    qewqe  = 1,
+    qweqeqeq = 2,
+    gjpeowgopiwegoje = 3,
+    gjpiogjejgier    = 5,
+    shgviwhoigw = 6
+}
+
+local t6 = {
+    qfrqfqfq    = 1, sjfowjolfw,
+    sjgvowjigjw = 2,
+    sfvhwieohgf = 3,
+    wgnowjgow=4
+}
+
+local t7 = {
+    --hhhh 
+    aaa = 1231,
+    bbbbb --sgjowjogg
+}
+
+local t8 = {
+    --[[sngkolwgjw]] fjhwofow = 1231,
+    sfowhfow = 1231,
+    fgnwogjpw = 6,
+    
+}
+
+local t9 = {
+    jfwejgwojgow--[[wkngowjgw]]  = 123131,
+    joiwejgjwgw                  = 13131,
+    fwhoifwjofgjw                = 353,
+    ljeogjeprjgpege              = 13131,
+}
--- Test/test_script/format_text_shouldbe/number.lua
+++ Test/test_script/format_text_shouldbe/number.lua
@@ -1,5 +1,5 @@
 local a = 1231 + 1231.123131 + 0xFFFF + 0.1e-30 + 0.9E-31
-    + 1e1 + 1E3
+    + 1e1 + 1E3 + 0x1.13aP3
 
 -- luajit
 local b = 1231ULL
