97c0501562b4755c9d8e637fcca777c638b81182
CppCXY <812125110@qq.com>
2022-01-19 15:33:30 +0800

    提交遗漏文件

--- include/Util/StringUtil.h
+++ include/Util/StringUtil.h
@@ -12,5 +12,10 @@ namespace StringUtil
 
 	std::string Replace(std::string_view source, std::string_view oldString, std::string_view newString);
 
+	// workaround for C++17
+	bool StartWith(const std::string& source, const std::string& str);
+	// workaround for C++17
+	bool EndWith(const std::string& source, const std::string& str);
+
 	bool IsStringEqualIgnoreCase(std::string_view lhs, std::string_view rhs);
 }
