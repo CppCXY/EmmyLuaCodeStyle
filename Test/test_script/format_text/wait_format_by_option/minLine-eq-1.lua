1972703a6cf1002454cc374348a72a173e5ec8cf
CppCXY <812125110@qq.com>
2022-02-02 19:37:57 +0800

    minLine基本测试

--- Test/test_script/format_text/wait_format_by_option/minLine-eq-1.lua
+++ Test/test_script/format_text/wait_format_by_option/minLine-eq-1.lua
@@ -1,16 +1,13 @@
 if aaa then
 
 end
-
 if bbb and ccc then
 
 end
-
 if ccc 
 and ccc then
 
 end
-
 function fff()
     if eee 
     or hhhh then
@@ -20,5 +17,12 @@ function fff()
     if aaa == bb 
     and cccc == bb then
     end
+    do return end
+    repeat local tt = 1231 until false
+    local t =1231
+    local dd =123
+    local eee= 12313131
+    dd= 131
 end
 
+ 
---
+++ Test/test_script/format_text/wait_format_by_option_should_be/minLine-eq-1.lua
@@ -0,0 +1,35 @@
+if aaa then
+
+end
+
+if bbb and ccc then
+
+end
+
+if ccc
+    and ccc then
+
+end
+
+function fff()
+    if eee
+        or hhhh then
+
+    end
+
+    if aaa == bb
+        and cccc == bb then
+    end
+
+    do return end
+
+    repeat local tt = 1231 until false
+
+    local t = 1231
+
+    local dd = 123
+
+    local eee = 12313131
+
+    dd = 131
+end
