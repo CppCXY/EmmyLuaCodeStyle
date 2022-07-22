588003250f1d3b0e252065f24eae45e3f4ece645
CppCXY <812125110@qq.com>
2022-07-22 10:42:59 +0800

    提供对齐调用参数新的可选值

---
+++ Test/test_script/format_text/wait_format_by_option/align_call_args-eq-only_after_more_indention_statement.lua
@@ -0,0 +1,25 @@
+callllll(aaaaaaaa,
+bbbb,
+cccccccc,
+ddddddddd,
+eeeeeeee,
+ffffffff)
+
+local f = fmt.format("aaaa"..
+"bbbbbbbbb"..
+"cccccc")
+
+table.insert(t, function()
+
+end)
+
+table.insert(t, create(function ()
+end))
+
+aaaaaaaaaaaaaa = calllll(aaa,bbbb,ccc,
+eeee,fheheh,heehe)
+
+return fmt.format(okokok,
+bbbb,
+cccc,eee,fff)
+
