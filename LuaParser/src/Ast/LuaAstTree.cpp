472dbaedefed7851c527aeb7a47430e1ddc83c2d
CppCXY <812125110@qq.com>
2022-10-10 20:51:27 +0800

    建树初步

--- LuaParser/src/Ast/LuaAstTree.cpp
+++ LuaParser/src/Ast/LuaAstTree.cpp
@@ -1,1 +1,5 @@
 #include "LuaParser/Ast/LuaAstTree.h"
+
+LuaAstTree::LuaAstTree(std::shared_ptr<LuaFile> file, std::vector<LuaToken> &&tokens, std::vector<MarkEvent> &&events) {
+
+}
--- include/LuaParser/Ast/LuaAstTree.h
+++ include/LuaParser/Ast/LuaAstTree.h
@@ -1,2 +1,21 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+#include "LuaParser/File/LuaFile.h"
+#include "LuaParser/Parse/Mark.h"
+#include "LuaParser/Lexer/LuaToken.h"
+
+class LuaAstTree {
+public:
+    LuaAstTree(std::shared_ptr<LuaFile> file,
+               std::vector<LuaToken> &&tokens,
+               std::vector<MarkEvent> &&events);
+
+    void BuildTree();
+
+private:
+    std::shared_ptr<LuaFile> _file;
+    std::vector<LuaToken> _tokens;
+    std::vector<MarkEvent> _events;
+};
