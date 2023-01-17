2b8a6602678f2a4a08186a1c9aa9f1031e1e942c
CppCXY <812125110@qq.com>
2023-01-17 17:49:21 +0800

    增加10万行代码格式化的性能测试

--- Test/src/Performance_unitest.cpp
+++ Test/src/Performance_unitest.cpp
@@ -1,6 +1,18 @@
 #include <gtest/gtest.h>
 #include "TestHelper.h"
 
+TEST(FormatPerformance, 1k_row) {
+    auto text = TestHelper::ReadFile("performance/1k_row_code.lua");
+    auto p = TestHelper::GetParser(text);
+    LuaSyntaxTree t;
+    t.BuildTree(p);
+
+    FormatBuilder b(TestHelper::DefaultStyle);
+    b.FormatAnalyze(t);
+    auto formatted = b.GetFormatResult(t);
+    EXPECT_TRUE(formatted.size() > 0);
+}
+
 TEST(FormatPerformance, 10k_row) {
     auto text = TestHelper::ReadFile("performance/10k_row_code.lua");
     auto p = TestHelper::GetParser(text);
@@ -10,4 +22,29 @@ TEST(FormatPerformance, 10k_row) {
     FormatBuilder b(TestHelper::DefaultStyle);
     b.FormatAnalyze(t);
     auto formatted = b.GetFormatResult(t);
+    EXPECT_TRUE(formatted.size() > 0);
+}
+
+TEST(FormatPerformance, 30k_row) {
+    auto text = TestHelper::ReadFile("performance/30k_row_code.lua");
+    auto p = TestHelper::GetParser(text);
+    LuaSyntaxTree t;
+    t.BuildTree(p);
+
+    FormatBuilder b(TestHelper::DefaultStyle);
+    b.FormatAnalyze(t);
+    auto formatted = b.GetFormatResult(t);
+    EXPECT_TRUE(formatted.size() > 0);
+}
+
+TEST(FormatPerformance, 100k_row) {
+    auto text = TestHelper::ReadFile("performance/100k_row_code.lua");
+    auto p = TestHelper::GetParser(text);
+    LuaSyntaxTree t;
+    t.BuildTree(p);
+
+    FormatBuilder b(TestHelper::DefaultStyle);
+    b.FormatAnalyze(t);
+    auto formatted = b.GetFormatResult(t);
+    EXPECT_TRUE(formatted.size() > 0);
 }
