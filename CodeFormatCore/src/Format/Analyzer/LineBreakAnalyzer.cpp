73cc1bc211b63f6014b05550e3af9ead750dcb0f
CppCXY <812125110@qq.com>
2023-10-19 14:39:13 +0800

    fix bug

--- CodeFormatCore/src/Format/Analyzer/LineBreakAnalyzer.cpp
+++ CodeFormatCore/src/Format/Analyzer/LineBreakAnalyzer.cpp
@@ -42,7 +42,7 @@ void LineBreakAnalyzer::ComplexAnalyze(FormatState &f, const LuaSyntaxTree &t) {
             switch (syntaxNode.GetSyntaxKind(t)) {
                 case LuaSyntaxNodeKind::Block: {
                     auto children = syntaxNode.GetChildren(t);
-                    if (syntaxNode.IsSingleLineNode(t)) {
+                    if (syntaxNode.GetParent(t).IsSingleLineNode(t)) {
                         if (children.size() <= 1) {
                             auto spaceAnalyzer = f.GetAnalyzer<SpaceAnalyzer>();
                             spaceAnalyzer->SpaceAround(syntaxNode, t);
